e516750cdb656640dd88935ff7463e8b1b5a4e3b
jjanickamatraszek <jjanickamatraszek@solvd.com>
2024-01-22 12:49:02 +0100

    pass entries directly to list without R.TESTDATA.get

--- src/test/java/ApiTests.java
+++ src/test/java/ApiTests.java
@@ -201,7 +201,8 @@ public class ApiTests implements IAbstractTest {
     @Test
     @TestCaseKey(value = "JOANNA-67")
     public void getSelectedEntriesFromGetAllPostTest() {
-        List<String> selectedEntries = List.of(R.TESTDATA.get("post.entry_1"), R.TESTDATA.get("post.entry_2"), R.TESTDATA.get("post.entry_3"));
+//        List<String> selectedEntries = List.of(R.TESTDATA.get("post.entry_1"), R.TESTDATA.get("post.entry_2"), R.TESTDATA.get("post.entry_3"));
+        List<String> selectedEntries = List.of("tags","reactions","body");
         GetAllPostsMethod getAllPostsMethodSelected = new GetAllPostsMethod(selectedEntries);
 
         AtomicInteger counter = new AtomicInteger(0);
